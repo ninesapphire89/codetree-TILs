c0ee06cda4555d88f0f1b5d9b736e3bee2fb7d36
ninesapphire89 <89dltjwls@gmail.com>
2024-07-20 12:45:17 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a%3==0)
+        printf("YES");
+    else
+        printf("NO");
+    printf("\n");
+    if (a%5==0)
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
+}
