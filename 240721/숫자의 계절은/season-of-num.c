3227de2a52a47276ad0f32bca2841cb68350521e
ninesapphire89 <89dltjwls@gmail.com>
2024-07-21 19:56:59 +0900

    Title: 숫자의 계절은, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/숫자의 계절은/season-of-num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a=12||a<=2){
+        printf("Winter");
+    }
+    else if (a>=9){
+        printf("Fall");
+    }
+    else if (a>=6){
+        printf("Summer");
+    }
+    else {
+        printf("Spring");
+    }
+    return 0;
+}
