e9c6e09eb24b0556eaef1961c94e89a4db162b00
ninesapphire89 <89dltjwls@gmail.com>
2024-07-21 20:09:00 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/장학금/scholarship.c
+++ 240721/장학금/scholarship.c
@@ -3,11 +3,9 @@
 int main() {
     int a,b;
     scanf("%d %d",&a,&b);
-    if (a<=90)
-        printf("0");
-    else if (b>=95)
+    if (a>=90&&b>=95)
         printf("100000");
-    else if (b>=90)
+    else if (a>=90&&b>=90)
         printf("50000");
     else
         printf("0");
