785009c9f11e642bea641b76caf1fddb2b0b9e9a
ninesapphire89 <89dltjwls@gmail.com>
2024-07-21 20:30:40 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/남녀노소 구분짓기/sex-and-age.c
+++ 240721/남녀노소 구분짓기/sex-and-age.c
@@ -4,16 +4,16 @@ int main() {
     int a,b;
     scanf("%d\n%d",&a,&b);
     if (a==0){
-                if (b>=19)
-                    printf("MAN");
-                else
-                    printf("BOY");
+        if (b>=19)
+            printf("MAN");
+        else
+            printf("BOY");
     }
     else{
-                if (b>=19)
-                    printf("WOMAN");
-                else
-                    printf("GIRL");
+        if (b>=19)
+            printf("WOMAN");
+         else
+            printf("GIRL");
     }
     return 0;
 }
