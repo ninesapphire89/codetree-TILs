79117f970ceb20238b0c8de87a988b9f44818789
ninesapphire89 <89dltjwls@gmail.com>
2024-07-25 10:45:21 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/369 게임/369-game.c
+++ 240725/369 게임/369-game.c
@@ -16,6 +16,42 @@ int main() {
         else if(90<=i&&i<100){
             printf("0 ");
         }
+        else if(10<i&&i<20){
+            if ((i-10)%3==0)
+                printf("0 ");
+            else    
+                printf("%d ",i);
+        }
+        else if(20<i&&i<30){
+            if ((i-20)%3==0)
+                printf("0 ");
+            else    
+                printf("%d ",i);
+        }
+        else if(40<i&&i<50){
+            if ((i-40)%3==0)
+                printf("0 ");
+            else    
+                printf("%d ",i);
+        }
+        else if(50<i&&i<60){
+            if ((i-50)%3==0)
+                printf("0 ");
+            else    
+                printf("%d ",i);
+        }
+        else if(70<i&&i<80){
+            if ((i-70)%3==0)
+                printf("0 ");
+            else    
+                printf("%d ",i);
+        }
+        else if(80<i&&i<90){
+            if ((i-80)%3==0)
+                printf("0 ");
+            else    
+                printf("%d ",i);
+        }
         else{
             printf("%d ",i);
         }
