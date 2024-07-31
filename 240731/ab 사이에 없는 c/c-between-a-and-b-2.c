6e48b2a1a93696f3d16b9d63ec879a34a0ab2996
ninesapphire89 <89dltjwls@gmail.com>
2024-07-31 15:16:28 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    bool of = false;
+    for (int i=a;i<=b;i++){
+        if (c%i==0){
+            of = true;
+        }
+    }
+    if(of==true){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
