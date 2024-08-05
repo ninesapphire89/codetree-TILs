e78ba125efcfd3e56f54feb1cce58177f9ce7fab
ninesapphire89 <89dltjwls@gmail.com>
2024-08-05 12:59:06 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/별 그리기/star-drawing.c
+++ 240805/별 그리기/star-drawing.c
@@ -3,23 +3,32 @@
 int main() {
     int n;
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        for(int j=1;j<n-i;j++){
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < n - i; j++) {
             printf(" ");
         }
-        for(int j=0;j<2*i+1;j++){
+        for(int j = 0; j < 2 * i - 1; j++) {
             printf("*");
+        }
+        for(int j = 0; j < n - i; j++) {
+            printf(" ");
         }
         printf("\n");
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<=i;j++){
+    
+    // Step 2:
+    for(int i = n - 1; i >= 1; i--) {
+        for(int j = 0; j < n - i; j++) {
             printf(" ");
         }
-        for(int j=0;j<n-2*i;j++){
+        for(int j = 0; j < 2 * i - 1; j++) {
             printf("*");
+        }
+        for(int j = 0; j < n - i; j++) {
+            printf(" ");
         }
         printf("\n");
     }
+
     return 0;
 }
