835c2c30a5ed7e920b906fc12e857dbbe42f9315
ninesapphire89 <89dltjwls@gmail.com>
2024-08-06 13:29:01 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/별표 출력하기 12/print-star-12.c
+++ 240806/별표 출력하기 12/print-star-12.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i==0||j%2==1){
+            if(i==0||(j%2==1&&i<=j)){
                 printf("* ");
             }
             else{
