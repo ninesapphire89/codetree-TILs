fae04d14bfcc1d3a2117be6cff88499b83f29df5
ninesapphire89 <89dltjwls@gmail.com>
2024-08-07 17:09:45 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/좌우 지그재그 출력/print-lr-zigzag.c
+++ 240807/좌우 지그재그 출력/print-lr-zigzag.c
@@ -2,21 +2,15 @@
 
 int main() {
     int n;
-    int cnt=1;
-    int cny=1;
     scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i%2==0){
-                printf("%d ",cnt);
-                cnt++;
-            }
-            else{
-                printf("%d ",3*i*n-cnt+1);
-                cnt++;
-            }
-        }
-        printf("\n");
+    for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			if(i % 2 == 0)
+				printf("%d ", (i * n) + j + 1);
+			else
+				printf("%d ", (i * n) + n - j);
+		}
+		printf("\n");
     }
     return 0;
 }
