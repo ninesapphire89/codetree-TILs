e5db1df87518fe1a464c4de11c624f0a4f0ab938
ninesapphire89 <89dltjwls@gmail.com>
2024-08-09 12:14:29 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            if(j==n-i+1){
+                printf("%d * %d = %d\n",i,j,i*j);
+            }
+            else{
+                printf("%d * %d = %d / ",i,j,i*j);
+            }
+        }
+    }
+    return 0;
+}
