dcc529043b5505d05cf4b598ef101194c8955568
ninesapphire89 <89dltjwls@gmail.com>
2024-08-09 12:08:32 +0900

    Title: 19단 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/19단 출력/nineteen-times-table.c
+++ 240809/19단 출력/nineteen-times-table.c
@@ -3,7 +3,7 @@
 int main() {
     for(int i=1;i<=19;i++){
         for(int j=1;j<=19;j++){
-            if(j%2==0){
+            if(j%2==0||j==19){
                 printf(" %d * %d = %d\n",i,j,i*j);
             }
             else{
