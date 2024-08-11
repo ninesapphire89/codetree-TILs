020b0b78ea3da422484f601e1bfdf72c6389c331
ninesapphire89 <89dltjwls@gmail.com>
2024-08-11 22:34:57 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/특정 숫자 도달하기/reaching-specific-number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[9];
+    int sum=0,avg_1=0;
+    double avg;
+    for(int i=0;i<10;i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]<250){
+            sum+=arr[i];
+            avg_1++;
+        }
+        else{
+            break;
+        }
+    }
+    avg=(double)sum/avg_1;
+    printf("%d %.1f",sum,avg);
+    return 0;
+}
