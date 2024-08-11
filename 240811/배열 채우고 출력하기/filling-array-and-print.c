0381926f9bc3365f600c690542d3d96caae37acb
ninesapphire89 <89dltjwls@gmail.com>
2024-08-11 22:23:32 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/배열 채우고 출력하기/filling-array-and-print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char arr[10];
+	
+    // 0부터 9까지의 인덱스에 주어진 문자를 차례대로 입력받습니다.
+    for(int i = 0; i < 10; i++) {
+        scanf(" %c", &arr[i]);
+    }
+	
+    // 9부터 0까지의 인덱스에 주어진 문자를 차례대로 출력합니다.
+    for(int i = 9; i >= 0; i--) {
+        printf("%c", arr[i]);
+    }
+	
+    return 0;
+}
