9f75c9f7b7111ba0ec15ac697dcb7bb8cc55d087
ninesapphire89 <89dltjwls@gmail.com>
2024-08-12 12:38:06 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/배열 채우기/filling-array.c
+++ 240812/배열 채우기/filling-array.c
@@ -2,14 +2,19 @@
 
 int main() {
     int arr[10];
-    for(int i=0;i<10;i++){
-        scanf("%d ",&arr[i]);
-        if(arr[i]==0){
-            for(int j=i-1;j>=0;j--){
-                printf("%d ",arr[j]);
-            }
+    int cnt = 0;
+
+    // 10개의 정수를 배열에 입력받으며 0이 있는지 확인합니다.
+    for(int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+        if(arr[i] == 0)
             break;
-        }
+        cnt++;
+    }
+
+    // 0이 입력되기 전까지의 수를 반대 순서로 출력합니다.
+    for(int i = cnt - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
