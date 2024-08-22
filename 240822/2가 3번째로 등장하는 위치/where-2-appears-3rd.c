a2c7c743e5ce59e7aa5e26c8688adfc80906b77e
ninesapphire89 <89dltjwls@gmail.com>
2024-08-22 13:08:07 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
+++ 240822/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -2,20 +2,33 @@
 
 int main() {
     int n;
-    int arr[n];
-    int con=0,tns=0;
-    scanf("%d\n",&n);
-    for(int i=1;i<=n;i++){
-        scanf("%d ",&arr[i]);
-        tns++;
-        if(arr[i]==2){
-            con++;
-        }
-        if(con==3){
-            printf("%d",tns);
-            break;
+    int count = 0;
+    int position = -1;
+    
+    // 입력 받기
+    scanf("%d", &n);
+    
+    int numbers[n];
+    
+    // 숫자 배열 입력 받기
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+    
+    // 숫자 2가 몇 번째로 등장하는지 찾기
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] == 2) {
+            count++;
+            if (count == 3) {
+                position = i + 1; // 위치는 1부터 시작하므로 i + 1
+                break;
+            }
         }
     }
-
+    
+    // 결과 출력
+    if (position != -1) {
+        printf("%d\n", position);
+    } 
     return 0;
 }
