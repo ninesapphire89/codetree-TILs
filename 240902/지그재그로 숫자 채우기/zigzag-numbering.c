f7f30b2cd214270f07e2227786034f7e7ce72fec
ninesapphire89 <89dltjwls@gmail.com>
2024-09-02 20:23:10 +0900

    Title: 지그재그로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240902/지그재그로 숫자 채우기/zigzag-numbering.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int arr[n][m];
+    int num=0;
+    for(int i=0;i<m;i++){
+        if(i%2==0){
+            for(int j=0;j<n;j++){
+                arr[j][i]=num;
+                num++;
+            }
+        }
+        else{
+            for(int j=n-1;j>=0;j--){
+                arr[j][i]=num;
+                num++;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
