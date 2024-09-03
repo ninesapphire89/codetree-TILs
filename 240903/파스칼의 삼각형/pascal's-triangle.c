ff9a4adaec5d2d42277fbd12c7531e147994ee47
ninesapphire89 <89dltjwls@gmail.com>
2024-09-03 13:31:29 +0900

    Title: 파스칼의 삼각형, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240903/파스칼의 삼각형/pascal's-triangle.c
+++ 240903/파스칼의 삼각형/pascal's-triangle.c
@@ -7,18 +7,12 @@ int main() {
     int arr[5][5];
 
     for(int i=0;i<n;i++){
-        arr[i][0]=1;
-    }
-
-
-
-    for(int i=1;i<n;i++){
-        for(int j=1;j<=i;j++){
-            if(i==j){
+        for(int j=0;j<=i;j++){
+            if(i==0||j==0||i==j){
                 arr[i][j]=1;
             }
             else{
-            arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
             }
         }
     }
