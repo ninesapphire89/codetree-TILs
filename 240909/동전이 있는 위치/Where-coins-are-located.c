ad94d73ab858aa55945db81a9989aa3d5c2c275e
ninesapphire89 <89dltjwls@gmail.com>
2024-09-09 22:44:22 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240909/동전이 있는 위치/Where-coins-are-located.c
+++ 240909/동전이 있는 위치/Where-coins-are-located.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int n,m;
-    scanf("%d %d",&n,&m);
-
-    int arr[m][n];
-
-    for(int i=0;i<m;i++){
-        int a,b;
-        scanf("%d %d",&a,&b);
-        arr[a][b]=1;
-    }
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            printf("%d ",arr[i][j]);
-        }
-        printf("\n");
-    }
+    int arr[10][10] = {0,};
+	
+	// n, m을 입력받습니다.
+	int n, m;
+	scanf("%d %d", &n, &m);
+	
+	// m회에 걸쳐 동전의 위치를 입력받고 올바른 위치에 1을 표기합니다.
+	for(int i = 0; i < m; i++) {
+		int r, c;
+		scanf("%d %d", &r, &c);
+		arr[r][c] = 1;
+	}
+			
+	// 채워진 배열을 출력합니다.
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= n; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
     return 0;
 }
