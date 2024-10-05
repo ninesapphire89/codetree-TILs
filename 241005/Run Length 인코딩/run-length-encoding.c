3b4f153c10374fb90f0ff24ef1bf6b6a43108cc7
ninesapphire89 <89dltjwls@gmail.com>
2024-10-05 19:00:10 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241005/Run Length 인코딩/run-length-encoding.c
+++ 241005/Run Length 인코딩/run-length-encoding.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
+
+char str_a[1001];
+char ans[2001];
+int len_a;
+int cnt;
+
 int main() {
+    scanf("%s", str_a);
 
-    char arr[1000];
+    len_a = strlen(str_a);
 
-    scanf("%s",arr);
+    // 변환
+    // 입력의 첫번째 값을 읽고 초기화합니다.
+    char curr_char = str_a[0];
+    int num_char = 1;
+    for(int i = 1; i < len_a; i++){
+        if(str_a[i] == curr_char){
+            // 지금까지의 문자와 같으면 연속된 문자 개수만 추가해 주고 넘어갑니다.
+            num_char++;
+        }
+        else {
+            // 지금까지의 문자와 다르면 새로운 문자로 바꿔줘야 합니다.
+            // 지금까지 세어온 curr_char와 num_char를 기록합니다.
+            ans[cnt++] = curr_char;
 
-    int len = strlen(arr);
+            char mystr[10];
+            sprintf(mystr, "%d", num_char);
+            strcat(ans, mystr);
+            cnt = strlen(ans);
 
-    int num=0;
-    for(int i=0;i<len;i++){
-        if(arr[i]!=arr[i+1]){
-            num+=2;
+            // curr_char와 num_char를 현재 값으로 초기화합니다.
+            curr_char = str_a[i];
+            num_char = 1;
         }
     }
-    printf("%d\n",num);
+    // 마지막 덩어리에 해당하는 curr_char와 num_char를 기록합니다.
+    ans[cnt++] = curr_char;
 
-    int a =1;
+    char mystr[10];
+    sprintf(mystr, "%d", num_char);
+    strcat(ans, mystr);
+    cnt = strlen(ans);
 
-    for(int i =0;i<len;i++){
-        if(arr[i] == arr[i+1]){
-            a++;
+    printf("%d\n%s", cnt, ans);
 
-        }
-        else{
-            printf("%c%d",arr[i],a);
-            a=1;
-        }
-    }
-    
     return 0;
 }
