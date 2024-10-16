18bf9cd1d1079a1288969512f11f6686efe08976
ninesapphire89 <89dltjwls@gmail.com>
2024-10-16 15:38:07 +0900

    Title: 문자열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241016/문자열의 특정 위치 찾기/find-specific-location-in-spring.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    char str[20];
+    char a;
+
+    scanf("%s",str);
+    scanf(" %c",&a);
+
+    int len = strlen(str);
+
+    int str_index = -1;
+
+    for(int i=0;i<(len-1);i++){
+        if(str[i]==a){
+            str_index = i;
+            break;
+        }
+    }
+
+    if(str_index == -1){
+        printf("No");
+    }
+    else{
+        printf("%d",str_index);
+    }
+
+    return 0;
+}
