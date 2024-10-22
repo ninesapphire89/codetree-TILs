7392b2d596b55155ee939a6bb080848294cd5acf
ninesapphire89 <89dltjwls@gmail.com>
2024-10-22 14:35:14 +0900

    Title: 부분 문자열의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/부분 문자열의 개수/number-of-substrings.c
+++ 241022/부분 문자열의 개수/number-of-substrings.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int count_substring_occurrences(const char *A, const char *B) {
+    int count = 0;
+    int lenA = strlen(A);
+    int lenB = strlen(B);
     
-    char str[1000], target[3];
-
-    scanf("%s",str);
-    scanf("%s",target);
-
-    int str_len = strlen(str);
-
-    int num = -1;
-
-    for(int i=0;i<str_len-1;i++){
-        for(int j=0;j<2;j++){
-            if(str[i+j]==target[j]&&str[i+j+1]==target[j+1]){
-                num++;
-            }
+    for (int i = 0; i <= lenA - lenB; i++) {
+        if (strncmp(&A[i], B, lenB) == 0) {
+            count++;
         }
     }
+    
+    return count;
+}
 
-    printf("%d",num);
+int main() {
+    char A[1001]; // A의 최대 길이 1000 + 널 종료 문자
+    char B[3];    // B의 길이는 2 + 널 종료 문자
+
+    // 입력 받기
+    scanf("%s", A);
+    scanf("%s", B);
+    
+    // 결과 출력
+    int result = count_substring_occurrences(A, B);
+    printf("%d\n", result);
 
     return 0;
 }
