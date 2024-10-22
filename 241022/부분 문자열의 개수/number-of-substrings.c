3f9be8a5c0e5c3808da46ea998a99907d42346ee
ninesapphire89 <89dltjwls@gmail.com>
2024-10-22 14:33:56 +0900

    Title: 부분 문자열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241022/부분 문자열의 개수/number-of-substrings.c
+++ 241022/부분 문자열의 개수/number-of-substrings.c
@@ -3,18 +3,17 @@
 
 int main() {
     
-    char str[1000], target[2];
+    char str[1000], target[3];
 
     scanf("%s",str);
     scanf("%s",target);
 
     int str_len = strlen(str);
-    int target_len = strlen(target);
 
-    int num = 0;
+    int num = -1;
 
-    for(int i=0;i<str_len;i++){
-        for(int j=0;j<target_len;j++){
+    for(int i=0;i<str_len-1;i++){
+        for(int j=0;j<2;j++){
             if(str[i+j]==target[j]&&str[i+j+1]==target[j+1]){
                 num++;
             }
