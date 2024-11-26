705b22b99e8fe8374c0166548ba202eb740dfe7b
ninesapphire89 <89dltjwls@gmail.com>
2024-11-26 14:26:09 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241126/대문자로 출력하기/print-in-capital.c
+++ 241126/대문자로 출력하기/print-in-capital.c
@@ -3,18 +3,18 @@
 #include <ctype.h>
 
 int main() {
-    
-    char line[101];
-    scanf("%s",line);
-
-    int len = strlen(line);
-    char result[101];
-
-    for(int i =0;i<len;i++){
-        if((int)'A'<=(int)line[i]&&(int)line[i]<=(int)'z'){
-            printf("%c",toupper(line[i]));
-        }
-    }
+    // 문자열을 정의합니다.
+	char str[101];
+	
+	// 문자열을 입력받습니다.
+  	scanf("%s", str);
+	
+	// 문자를 하나하나 확인하여 알파벳일 경우 모두 대문자로 출력합니다.
+	for(int i = 0; str[i] != '\0'; i++) {
+		if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+			printf("%c", toupper(str[i]));
+		}
+	}
 
     return 0;
 }
