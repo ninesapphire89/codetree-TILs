89d17b4484d059caa05397dc24382740dbd2c0d4
ninesapphire89 <89dltjwls@gmail.com>
2025-10-08 22:42:55 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251008/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a,b,c;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    int avg = (a+b+c)/3;
+
+    printf("%d\n%d\n%d",a+b+c,avg,a+b+c-avg);
+
+    return 0;
+}
