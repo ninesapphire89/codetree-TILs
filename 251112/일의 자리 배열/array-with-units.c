d7848967289a576eee049d5853272473f23ddac8
ninesapphire89 <89dltjwls@gmail.com>
2025-11-12 22:11:45 +0900

    Title: 일의 자리 배열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251112/일의 자리 배열/array-with-units.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arr[10];
+
+    scanf("%d %d",&arr[0],&arr[1]);
+
+    printf("%d %d ",arr[0],arr[1]);
+
+    for(int i=2;i<10;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+
+        if(arr[i]>=10){
+            arr[i]=arr[i]-10;
+        }
+
+        printf("%d ",arr[i]);
+
+    }
+
+    return 0;
+}
